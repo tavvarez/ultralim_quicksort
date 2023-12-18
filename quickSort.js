25c1c44488ce03ea24a0c90929014cc861554346
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-12-18 20:51:11 -0300

    slice e filter na lista que é menor ou igual que o pivot

--- quickSort.js
+++ quickSort.js
@@ -1,3 +1,8 @@
-function ordenacaoQuickSort() {
-    
+function ordenacaoQuickSort(array) {
+    if (array.lenght <= 1) {
+        return array
+    } else {
+        const pivot = array[0]
+        const menorQuePivot = array.slice(1).filter(x => x <= pivot)
+    }
 }
