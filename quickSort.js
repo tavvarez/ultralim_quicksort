efcaa257e0b73cfaffe47a8a1ce6711cbb036c3d
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-12-18 21:02:35 -0300

    finish

--- quickSort.js
+++ quickSort.js
@@ -1,8 +1,16 @@
-function ordenacaoQuickSort(array) {
-    if (array.lenght <= 1) {
-        return array
+function ordenacaoQuickSort(arrayNumeros) {
+    if (arrayNumeros.length <= 1) {
+        return arrayNumeros
     } else {
-        const pivot = array[0]
-        const menorQuePivot = array.slice(1).filter(x => x <= pivot)
+        const pivot = arrayNumeros[0]
+        const menorQuePivot = arrayNumeros.slice(1).filter(x => x <= pivot)
+        const maiorQuePivot = arrayNumeros.slice(1).filter(x => x > pivot)
+        return [...ordenacaoQuickSort(menorQuePivot), ...ordenacaoQuickSort(maiorQuePivot)]
     }
 }
+
+let listaDesordenada = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+let listaOrdenada = ordenacaoQuickSort(listaDesordenada)
+
+console.log("Lista desordenada: " + listaDesordenada)
+console.log("Lista ordenada: " + listaOrdenada)
